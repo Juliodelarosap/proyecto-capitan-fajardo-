2c441bddbfddb4dd4e4dcc24d1494ccbb7b082b3
Jorge Luis <114448996+Jolitechrdcoder@users.noreply.github.com>
2024-02-06 10:43:53 -0500

    Create header.php

---
+++ rafatienda2/layouts/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- mis estilos css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- bostrap cdn css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <!-- fontawesome cdn  -->
+   <script src="https://kit.fontawesome.com/6b54a05747.js" crossorigin="anonymous"></script>
+
+    <title>RFtecnology</title>
+</head>
+<body>
+    <!-- barra de navegacion superior o navbar  -->
+ <nav class="navbar navbar-expand-lg navbar-light bg-white  py-3 fixed-top">
+    <div class="container">
+      <img  src="assets/imgs/logo.png" alt="">
+      <!-- <h2 class="brand">RAFATECHNOLOGY</h2> -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse  nav-buttons " id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="tienda.php">Tienda</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+         
+
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+          
+          <li class="nav-item">
+            <a href="carrito.php"><i class="fas fa-shopping-bag"></i></a>
+            <a href="cuenta.php"><i class="fas fa-user"></i></a>
+          </li>
+          
+        </ul>
+        
+      </div>
+    </div>
+  </nav>
