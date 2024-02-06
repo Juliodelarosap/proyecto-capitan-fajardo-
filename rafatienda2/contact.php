3383270dfa2e1f72cb39b335a9b7d26d538ddb88
Jorge Luis <114448996+Jolitechrdcoder@users.noreply.github.com>
2024-02-06 10:44:02 -0500

    Create contact.php

---
+++ rafatienda2/contact.php
@@ -0,0 +1,26 @@
+
+      
+      
+      <?php include('layouts/header_contact.php') ?> 
+      <!-- pagina de contacto -->
+   
+      <section id="contact" class="container my-5 py-5">
+        <div class="container text-center mt-5">
+         <h3>Contactanos</h3>
+         <hr class="mx-auto">
+         <p class="w-50 mx-auto">
+             Tel: <span>+1809-856-2723</span>
+         </p>
+         <p class="w-50 mx-auto">
+            Correo: <span>rftechnologydr@gmail.com </span>
+        </p>
+        <p class="w-50 mx-auto">
+            Trabajamos 24/7 para responder todas tus preguntas.
+        </p>
+        </div>
+      </section>
+   
+
+     
+
+      <?php include('layouts/footer.php') ?> 
