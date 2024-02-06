91fd2ec85a0dcd1139986142a19acfb787588d63
Jorge Luis <114448996+Jolitechrdcoder@users.noreply.github.com>
2024-02-06 10:43:16 -0500

    Create conection.php

---
+++ rafatienda2/server/conection.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$clave = "";
+$base_de_datos = "php_rafa";
+
+$conn = mysqli_connect($host, $usuario, $clave, $base_de_datos)
+        or die("no se conecto la base de datos");
+
+ 
+?>
