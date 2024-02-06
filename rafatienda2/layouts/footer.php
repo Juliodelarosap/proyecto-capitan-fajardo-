bcf3381e15b23fa2c2905aa631c7594bcf49906d
Jorge Luis <114448996+Jolitechrdcoder@users.noreply.github.com>
2024-02-06 10:43:55 -0500

    Create footer.php

---
+++ rafatienda2/layouts/footer.php
@@ -0,0 +1,73 @@
+<footer class="mt-5 py-5">
+       <div class="row container mx-auto pt-5">
+        <div class=" footer-one col-lg-3 col-md-6 col-sm-12">
+         <img class="logo" src="assets/imgs/icono.png" alt="">
+         <p class="pt-3">Ofrecemos los mejores productos a los precios más asequibles.</p>
+        </div>
+        
+        <div class=" footer-one col-lg-3 col-md-6 col-sm-12">
+           <h5 class="pb-2">Featured</h5>
+           <ul class="text-uppercase">
+            <li><a href="#">Hombre</a></li>
+            <li><a href="#">Mujer</a></li>
+            <li><a href="#">Ninos</a></li>
+            <li><a href="#">Nuevos Productos</a></li>
+            <li><a href="#">Mochilas</a></li>
+           </ul>
+           </div>
+
+           <div class=" footer-one col-lg-3 col-md-6 col-sm-12">   
+            <h5 class="pb-2">Contacto</h5>
+            <div>
+                <h6 class="text-uppercase">Direccion</h6>
+                <p>Santo Domingo,Distrito nacional</p>
+            </div>
+            <div>
+                <h6 class="text-uppercase">Telefonos</h6>
+                <p> Cel:+1809-856-2723</p>
+            </div>
+            <div>
+                <h6 class="text-uppercase">Correo</h6>
+                <p>rftechnologydr@gmail.com</p>
+            </div>
+           </div>
+
+           
+       </div>
+
+        <div class="copyright mt-5">
+            <div class="row container mx-auto">
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+                    <img src="assets/imgs/pay.jpg" />
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-4 text-nowrap mb-2">
+                    <p>Rftechnology @ 2025 All Right Reserved</p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-whatsapp"></i></a>
+                </div>
+
+            </div>
+        </div>
+
+
+
+     </footer>
+
+
+
+
+
+
+
+
+
+
+
+    <!-- bostrap cdn js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
